55abc3a03292393a1cabe9a85ecac9c9e212dca0
Tanmai Gopal <tanmaig@34cross.in>
2018-10-29 20:16:30 -0700

    deploy live

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,10 +4,20 @@ import AuthorList from "../components/AuthorList"
 const Index = ({ data }) => (
   <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100vh'}}>
     <div style={{marginTop: '-100px'}}>
-      <h1> Hey there  </h1>
-      <img src="https://media1.tenor.com/images/520e101b7d01ee075a732bf4dcd2d63b/tenor.gif?itemid=4458651"/>
+      <h1> Author List</h1>
+      <AuthorList authors={data.hasura.author} />
     </div>
   </div>
 )
 
 export default Index;
+
+export const query = graphql`
+  query {
+    hasura {
+      author {
+        id
+        name
+      }
+    }
+  }`;
