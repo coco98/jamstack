4b36ac7a0e2a1d46a5b093ac9389428fd6e665f3
Tanmai Gopal <tanmaig@34cross.in>
2018-11-03 01:26:20 -0700

    adds nodemailer

--- functions/hello.js
+++ functions/hello.js
@@ -1,9 +1,19 @@
-// import nodemailer from 'nodemailer';
+import nodemailer from 'nodemailer';
 
 exports.handler = (event, context, cb) => {
   console.log(event);
-  cb(null, {
-    statusCode: 200,
-    body: JSON.stringify({msg: 'Hello world'})
+  let transporter = nodemailer.createTransport({sendmail: true});
+  transporter.sendMail({
+      from: 'tanmai-netlify-test@tanmaigopal.com',
+      to: 'tanmaig@hasura.io',
+      subject: 'netlify message',
+      text: 'I hope this message gets delivered!'
+  }, (err, info) => {
+      cb(null, {
+        statusCode: 200,
+        body: JSON.stringify({msg: 'Hello world'})
+      });
+      console.log(info.envelope);
+      console.log(info.messageId);
   });
 };
